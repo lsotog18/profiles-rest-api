1124743d93bcf994097dc559c58668161e8fa6b4
lsotog18 <lsoto@istmocenter.com>
2021-10-10 12:10:53 -0600

    Enable django admin for user profiles

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,16 +4,17 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
-class UserProfilesManager(BaseUserManager):
-    """Manager for user Profiles"""
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
-            raise ValueError('User must have an email adress')
+            raise ValueError('Users must have an email address')
 
-        email= self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -21,10 +22,11 @@ class UserProfilesManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
-        """Create and save a new a superuser with given details"""
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
+
         user.is_superuser = True
-        user_is_staff = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
@@ -36,13 +38,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserProfilesManager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
@@ -50,5 +52,5 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.name
 
     def __str__(self):
-        """Retrieve string representation of our user"""
+        """Return string representation of user"""
         return self.email
